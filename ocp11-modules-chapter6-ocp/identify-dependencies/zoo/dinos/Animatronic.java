94e8e5469813b47eeba0836a0ccf8b9af1e9746d
GeorgeCiachir <ciachirg@ebrd.com>
2021-03-31 08:31:55 +0300

    cyclic dependencies examples

---
+++ ocp11-modules-chapter6-ocp/identify-dependencies/zoo/dinos/Animatronic.java
@@ -0,0 +1,21 @@
+package zoo.dinos;
+
+import sun.misc.Unsafe;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Animatronic {
+
+    private List<String> names;
+    private LocalDate visitDate;
+
+    public Animatronic(List<String> names, LocalDate visitDate) {
+        this.names = names;
+        this.visitDate = visitDate;
+    }
+
+    public void unsafeMethod() {
+        Unsafe unsafe = Unsafe.getUnsafe();
+    }
+}
